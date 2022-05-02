274b8680cb0404fd7b2c0d2134445c0b571d29db
kwlaial <85923968+kwlaial@users.noreply.github.com>
2022-05-02 18:59:58 +0800

    Update BPM.ino

--- BPM/BPM.ino
+++ BPM/BPM.ino
@@ -1,24 +1,25 @@
-int ecgPin = 0;
-int upperThreshold = 100;
-int lowerThreshold = 0;
-int ecgOffset = 400;
+int upperThreshold = 2000;
+int lowerThreshold = 800;
+int ecgOffset = 8000;
 float beatsPerMinute = 0.0;
 bool alreadyPeaked = false;
 unsigned long firstPeakTime = 0;
 unsigned long secondPeakTime = 0;
 unsigned long rrInterval = 0;
+float LPF = 0.0;
+float LPF_beat = 0.0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() { 
-  int ecgReading = analogRead(ecgPin) - ecgOffset; 
-  // Measure the ECG reading minus an offset to bring it into the same
-  // range as the heart rate (i.e. around 60 to 100 bpm)
-
-  if (ecgReading > upperThreshold && alreadyPeaked == false) { 
+  int ecgReading = analogRead(ecgPin)*500 + ecgOffset; 
+  float weight = 0.1;                               
+  LPF = (1.0 - weight) * LPF + weight * ecgReading;
+  
+  if (LPF > upperThreshold && alreadyPeaked == false) { 
   // Check if the ECG reading is above the upper threshold and that
   // we aren't already in an existing peak
       if (firstPeakTime == 0) {
@@ -39,20 +40,26 @@ void loop() {
       alreadyPeaked = true;
   }
 
-  if (ecgReading < lowerThreshold) {
+  if (LPF < lowerThreshold) {
   // Check if the ECG reading has fallen below the lower threshold
   // and if we are ready to detect another peak
     alreadyPeaked = false;
     digitalWrite(LED_BUILTIN, LOW);
   }  
 
-  // Calculate the beats per minute, rrInterval is measured in
-  // milliseconds so we must multiply by 1000
   beatsPerMinute = (1.0/rrInterval) * 60.0 * 1000;
 
+
   // Print the final values to be read by the serial plotter
-  Serial.print(ecgReading);
-  Serial.print(",");         
-  Serial.println(beatsPerMinute);        
-  delay(10);
+  //Serial.print("ECG: ");
+  //Serial.println(LPF);
+  //Serial.print(",");
+  if (beatsPerMinute >= 60 && beatsPerMinute <= 140){
+      float weight = 0.1;                               
+      LPF_beat = (1.0 - weight) * LPF_beat + weight * beatsPerMinute;
+    Serial.print("BPM: ");         
+    Serial.println(LPF_beat);    
+  }
+    
+  delay(5);
 }
